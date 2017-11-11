8f8f5f4261e1372a156cd2c7935c5aebb4f21903
Alexander Zacherl <alexander.zacherl@googlemail.com>
2017-11-11 22:21:42 +0100

    improved unit spawn preview

--- Assets/Gameplay.cs
+++ Assets/Gameplay.cs
@@ -13,11 +13,12 @@ public class Gameplay : MonoBehaviour {
     public int UnitSelected = 0;
     public float SpawnDistance = 15f;
     public GameObject Champion;
+    public GameObject up;
 
     // Use this for initialization
     void Start () {
-		
-	}
+        up.SetActive(false);
+    }
 
     void SpawnUnit(int unitID)
     {
@@ -40,15 +41,20 @@ public class Gameplay : MonoBehaviour {
             {
                 case 1:
                     Instantiate(U1pref, worldPos, Quaternion.identity);
+                    UnitSelected = 0;
+                    up.SetActive(false);
                     break;
                 case 2:
                     Instantiate(U2pref, worldPos, Quaternion.identity);
+                    UnitSelected = 0;
+                    up.SetActive(false);
                     break;
                 case 3:
                     Instantiate(U3pref, worldPos, Quaternion.identity);
+                    UnitSelected = 0;
+                    up.SetActive(false);
                     break;
                 default:
-                    print("No unit selected.");
                     break;
             }
         }
@@ -90,9 +96,11 @@ public class Gameplay : MonoBehaviour {
             if (UnitSelected == 1)
             {
                 UnitSelected = 0;
+                up.SetActive(false);
             }
             else {
                 UnitSelected = 1;
+                up.SetActive(true);
             }     
         }
         else if (Input.GetKeyDown("2"))
@@ -100,10 +108,12 @@ public class Gameplay : MonoBehaviour {
             if (UnitSelected == 2)
             {
                 UnitSelected = 0;
+                up.SetActive(false);
             }
             else
             {
                 UnitSelected = 2;
+                up.SetActive(true);
             }
         }
         else if (Input.GetKeyDown("3"))
@@ -111,13 +121,33 @@ public class Gameplay : MonoBehaviour {
             if (UnitSelected == 3)
             {
                 UnitSelected = 0;
+                up.SetActive(false);
             }
             else
             {
                 UnitSelected = 3;
+                up.SetActive(true);
             }
         }
 
-
+        Vector3 thisPos;
+        Ray thisray = Camera.main.ScreenPointToRay(mousePos);
+        RaycastHit thishit;
+        if (Physics.Raycast(thisray, out thishit, 1000f))
+        {
+            thisPos = thishit.point;
+        }
+        else
+        {
+            thisPos = Camera.main.ScreenToWorldPoint(mousePos);
+        }
+        if (Vector3.Distance(thisPos, Champion.transform.position) <= SpawnDistance)
+        {
+            up.gameObject.GetComponent<Renderer>().material.SetColor("_Color", Color.white);
+        }
+        else
+        {
+            up.gameObject.GetComponent<Renderer>().material.SetColor("_Color", Color.black);
+        }
     }
 }
