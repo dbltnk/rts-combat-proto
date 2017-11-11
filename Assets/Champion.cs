7f655f1c1b1e706c4510a7bfa8db31f6151ab355
Alexander Zacherl <alexander.zacherl@googlemail.com>
2017-11-11 16:11:38 +0100

    can spawn unit in circle

--- Assets/Champion.cs
+++ Assets/Champion.cs
@@ -6,16 +6,36 @@ public class Champion : MonoBehaviour {
 
     public float speed = 1f;
     public Vector3 TargetPos;
+    public Gameplay gp;
+    public GameObject Circle;
 
     // Use this for initialization
     void Start () {
+        gp = (Gameplay)FindObjectOfType(typeof(Gameplay));
         TargetPos = transform.position;
-
+        Circle = this.gameObject.transform.GetChild(1).gameObject;
     }
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
+        Move();
+        DoCircle();
+    }
 
+private void DoCircle()
+{
+    if (gp.UnitSelected == 0)
+        {
+            Circle.gameObject.SetActive(false);
+        }
+    else {
+            Circle.gameObject.SetActive(true);
+        }
+}
+    
+private void Move()
+    {
         Vector3 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0f);
 
         if (Input.GetMouseButtonDown(1))
@@ -35,5 +55,4 @@ public class Champion : MonoBehaviour {
         float step = speed * Time.deltaTime;
         transform.position = Vector3.MoveTowards(transform.position, TargetPos, step);
     }
-
 }
--- Assets/Gameplay.cs
+++ Assets/Gameplay.cs
@@ -6,15 +6,58 @@ public class Gameplay : MonoBehaviour {
 
     public bool HasStarted = false;
     public GameObject ChampionPrefab;
+    public GameObject U1pref;
+    public GameObject U2pref;
+    public GameObject U3pref;
     public GameObject ChampionPreviewObject;
+    public int UnitSelected = 0;
+    public float SpawnDistance = 15f;
+    public GameObject Champion;
 
     // Use this for initialization
     void Start () {
 		
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+    void SpawnUnit(int unitID)
+    {
+        Vector3 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0f);
+        Vector3 worldPos;
+        Ray ray = Camera.main.ScreenPointToRay(mousePos);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 1000f))
+        {
+            worldPos = hit.point;
+        }
+        else
+        {
+            worldPos = Camera.main.ScreenToWorldPoint(mousePos);
+        }
+
+        if(Vector3.Distance(worldPos, Champion.transform.position) <= SpawnDistance)
+        {
+            switch (unitID)
+            {
+                case 1:
+                    Instantiate(U1pref, worldPos, Quaternion.identity);
+                    break;
+                case 2:
+                    Instantiate(U2pref, worldPos, Quaternion.identity);
+                    break;
+                case 3:
+                    Instantiate(U3pref, worldPos, Quaternion.identity);
+                    break;
+                default:
+                    print("No unit selected.");
+                    break;
+            }
+        }
+
+        
+    }
+
+    // Update is called once per frame
+    void Update () {
         Vector3 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0f);
 
         if (Input.GetMouseButtonDown(0)) {
@@ -33,18 +76,48 @@ public class Gameplay : MonoBehaviour {
                     worldPos = Camera.main.ScreenToWorldPoint(mousePos);
                 }
                 GameObject.DestroyImmediate(ChampionPreviewObject);
-                Instantiate(ChampionPrefab, worldPos, Quaternion.identity);
+                Champion = Instantiate(ChampionPrefab, worldPos, Quaternion.identity);
                 HasStarted = true;
+            }
+            else
+            {
+                SpawnUnit(UnitSelected);
             }
         }
 
-
-        
-
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetKeyDown("1"))
         {
-           
+            if (UnitSelected == 1)
+            {
+                UnitSelected = 0;
+            }
+            else {
+                UnitSelected = 1;
+            }     
         }
+        else if (Input.GetKeyDown("2"))
+        {
+            if (UnitSelected == 2)
+            {
+                UnitSelected = 0;
+            }
+            else
+            {
+                UnitSelected = 2;
+            }
+        }
+        else if (Input.GetKeyDown("3"))
+        {
+            if (UnitSelected == 3)
+            {
+                UnitSelected = 0;
+            }
+            else
+            {
+                UnitSelected = 3;
+            }
+        }
+
 
     }
 }
