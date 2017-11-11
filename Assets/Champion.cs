2c4d2e446075bf8a627c391cfd1f2c771d007110
Alexander Zacherl <alexander.zacherl@googlemail.com>
2017-11-11 15:37:35 +0100

    champions moves to RMB

--- Assets/Champion.cs
+++ Assets/Champion.cs
@@ -4,13 +4,36 @@ using UnityEngine;
 
 public class Champion : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    public float speed = 1f;
+    public Vector3 TargetPos;
+
+    // Use this for initialization
+    void Start () {
+        TargetPos = transform.position;
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+
+        Vector3 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0f);
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(mousePos);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 1000f))
+            {
+                TargetPos = hit.point;
+            }
+            else
+            {
+                TargetPos = Camera.main.ScreenToWorldPoint(mousePos);
+            }
+        }
+
+        float step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, TargetPos, step);
+    }
+
 }
