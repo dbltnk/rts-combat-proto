5d1abb00871942c8a94194e1e3ba0d685c3931ab
Alexander Zacherl <alexander.zacherl@googlemail.com>
2017-11-11 14:46:12 +0100

    instantiate champion

---
+++ Assets/Gameplay.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gameplay : MonoBehaviour {
+
+    public bool HasStarted = false;
+    public GameObject ChampionPrefab;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0f);
+
+        if (Input.GetMouseButtonDown(0)) {
+
+            if (!HasStarted)
+            {
+                Vector3 worldPos;
+                Ray ray = Camera.main.ScreenPointToRay(mousePos);
+                RaycastHit hit;
+                if (Physics.Raycast(ray, out hit, 1000f))
+                {
+                    worldPos = hit.point;
+                }
+                else
+                {
+                    worldPos = Camera.main.ScreenToWorldPoint(mousePos);
+                }
+                Instantiate(ChampionPrefab, worldPos, Quaternion.identity);
+                HasStarted = true;
+            }
+        }
+
+
+        
+
+        if (Input.GetMouseButtonDown(0))
+        {
+           
+        }
+
+    }
+}
