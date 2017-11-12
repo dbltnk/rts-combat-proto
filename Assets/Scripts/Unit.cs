45ac618dd318b6395d122e9ff3da82d2d2746a3b
Alexander Zacherl <alexander.zacherl@googlemail.com>
2017-11-12 11:18:04 +0100

    supports multiple enemies

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -5,14 +5,12 @@ using UnityEngine;
 public class Unit : MonoBehaviour {
 
     private GameObject PFX;
-    private GameObject Target;
     public float Speed = 7f;
     private bool isDoneSpawning = false;
 
     void Start ()
     {
         PFX = this.gameObject.transform.GetChild(1).gameObject;
-        Target = GameObject.Find("Target");
         StartCoroutine(StopPFX());
     }
 
@@ -31,6 +29,26 @@ public class Unit : MonoBehaviour {
     private void Move()
     {
         float step = Speed * Time.deltaTime;
-        transform.position = Vector3.MoveTowards(transform.position, Target.transform.position, step);
+        transform.position = Vector3.MoveTowards(transform.position, FindClosestEnemy().transform.position, step);
+    }
+
+    public GameObject FindClosestEnemy()
+    {
+        GameObject[] gos;
+        gos = GameObject.FindGameObjectsWithTag("Enemy");
+        GameObject closest = null;
+        float distance = Mathf.Infinity;
+        Vector3 position = transform.position;
+        foreach (GameObject go in gos)
+        {
+            Vector3 diff = go.transform.position - position;
+            float curDistance = diff.sqrMagnitude;
+            if (curDistance < distance)
+            {
+                closest = go;
+                distance = curDistance;
+            }
+        }
+        return closest;
     }
 }
